cb41e5a01358696664075a29688405a3146801ae
Marek Skopowski <marek.skopowski@flashlabs.pl>
2024-02-02 19:12:52 +0100

    Add static error to test suite

--- internal/handler/message/create/handle_test.go
+++ internal/handler/message/create/handle_test.go
@@ -3,6 +3,7 @@ package create_test
 import (
 	"bytes"
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -24,7 +25,41 @@ var (
 		"mailing_id": 1,
 		"insert_time": "2020-04-24T05:42:38.725412916Z"
 	}`)
+
+	ErrDbError = errors.New("database error")
 )
+
+func TestCreateHandleCompletesWithError(t *testing.T) {
+	p, err := pgxmock.NewPool()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer p.Close()
+
+	p.ExpectExec("INSERT INTO message").
+		WithArgs(pgxmock.AnyArg(), pgxmock.AnyArg(), pgxmock.AnyArg(), pgxmock.AnyArg(), pgxmock.AnyArg(), pgxmock.AnyArg()).
+		WillReturnError(ErrDbError)
+
+	registry.DBPool = p
+
+	r, err := initializer.Router()
+	require.NoError(t, err)
+
+	err = initializer.Handler(r)
+	require.NoError(t, err)
+
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, "/api/messages", bytes.NewBuffer(payload))
+	require.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusInternalServerError, rr.Code)
+
+	if err = p.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
 
 func TestCreateHandleCompletesSuccessfully(t *testing.T) {
 	p, err := pgxmock.NewPool()
